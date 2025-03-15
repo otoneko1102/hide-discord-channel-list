c6eaee12cf8d55bfb426e0c7642470c8bb2d9816
otoneko1102 <pineapple2006.11.2@gmail.com>
2025-03-15 10:27:01 +0900

    システム通知

---
+++ background.js
@@ -0,0 +1,9 @@
+chrome.runtime.onInstalled.addListener((details) => {
+  if (details.reason === "install") {
+    chrome.tabs.create({ url: "https://discord.gg/yKW8wWKCnS" });
+  }
+
+  if (details.reason === "update") {
+    chrome.tabs.create({ url: "https://discord.gg/yKW8wWKCnS" });
+  }
+});
