c7bf292f97bac66b1f74d5da8cd5443d18eef0e9
otoneko1102 <pineapple2006.11.2@gmail.com>
2025-03-15 19:31:12 +0900

    修正

--- background.js
+++ background.js
@@ -1,9 +1,9 @@
 chrome.runtime.onInstalled.addListener((details) => {
   if (details.reason === "install") {
-    chrome.tabs.create({ url: "https://discord.gg/yKW8wWKCnS" });
+    // chrome.tabs.create({ url: "https://discord.gg/yKW8wWKCnS" });
   }
 
   if (details.reason === "update") {
-    chrome.tabs.create({ url: "https://discord.gg/yKW8wWKCnS" });
+    // chrome.tabs.create({ url: "https://discord.gg/yKW8wWKCnS" });
   }
 });
